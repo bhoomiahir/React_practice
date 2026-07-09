17e26d4dbb02ea76f7d2a614af645c88a0ebab5e
Bhoomi Ahir <bhoomiahir.work@gmail.com>
2026-07-09 23:21:47 +0530

    useState logic added

--- State-Management/UseState/src/App.jsx
+++ State-Management/UseState/src/App.jsx
@@ -4,12 +4,24 @@ import { useState } from "react";
 function App() {
   const [Count, setCount] = useState(0);
 
+  function increaseCount() {
+    setCount(Count + 1);
+  }
+
+  function decreaseCount() {
+    setCount(Count - 1);
+  }
+
   return (
     <div className="Container">
       <h1>Counter</h1>
       <p>Count is : {Count}</p>
-      <button className="btn">increase</button>
-      <button className="btn">decrease</button>
+      <button className="btn" value="increase" onClick={increaseCount}>
+        increase
+      </button>
+      <button className="btn" value="decrease" onClick={decreaseCount}>
+        decrease
+      </button>
     </div>
   );
 }
