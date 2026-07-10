69d6ab37cc013c7b779047a81f0657aad4e002c4
Bhoomi Ahir <bhoomiahir.work@gmail.com>
2026-07-10 13:01:49 +0530

    reset btn functionality added

--- State-Management/UseState/src/App.jsx
+++ State-Management/UseState/src/App.jsx
@@ -12,15 +12,24 @@ function App() {
     setCount(Count - 1);
   }
 
+  const resetBtn = () => {
+    setCount(0);
+  };
+
   return (
     <div className="Container">
       <h1>Counter</h1>
       <p>Count is : {Count}</p>
-      <button className="btn" value="increase" onClick={increaseCount}>
-        increase
-      </button>
-      <button className="btn" value="decrease" onClick={decreaseCount}>
-        decrease
+      <div>
+        <button className="btn" value="increase" onClick={increaseCount}>
+          increase
+        </button>
+        <button className="btn" value="decrease" onClick={decreaseCount}>
+          decrease
+        </button>
+      </div>
+      <button className="btn rstbtn" onClick={resetBtn}>
+        Reset
       </button>
     </div>
   );
