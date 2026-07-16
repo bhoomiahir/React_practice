f526ab62a2386cfb21e1351fcc81fb41fb103ef1
Bhoomi Ahir <bhoomiahir.work@gmail.com>
2026-07-16 23:17:30 +0530

    Profile-card designed

---
+++ Props-Lists/ProfileCard/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Profilecard from "./Component/ProfileCard";
+
+function App() {
+  return <Profilecard />;
+}
+
+export default App;
---
+++ Props-Lists/ProfileCard/src/Component/ProfileCard.jsx
@@ -0,0 +1,15 @@
+function Profilecard() {
+  return (
+    <div className="profile-card">
+      <h2>ProfileCard</h2>
+      <img
+        src="https://i.pinimg.com/736x/e7/87/41/e7874123ba5b80d01e6bc1e58e545d3e.jpg"
+        alt="Hinata"
+      />
+      <p>Name: Tachibana Hinata</p>
+      <p>Role: girlfriend of Takemichi</p>
+    </div>
+  );
+}
+
+export default Profilecard;
