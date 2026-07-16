9d566e1a98934fe509fa97d724f7d7019ee42c0d
Bhoomi Ahir <bhoomiahir.work@gmail.com>
2026-07-16 23:26:55 +0530

    taking values from props

--- Props-Lists/ProfileCard/src/App.jsx
+++ Props-Lists/ProfileCard/src/App.jsx
@@ -2,7 +2,14 @@ import "./App.css";
 import Profilecard from "./Component/ProfileCard";
 
 function App() {
-  return <Profilecard />;
+  const profile = {
+    name: "Tachibana Hinata",
+    role: "girlfriend of Takemichi",
+    avatar:
+      "https://i.pinimg.com/736x/e7/87/41/e7874123ba5b80d01e6bc1e58e545d3e.jpg",
+  };
+
+  return <Profilecard profile={profile} />;
 }
 
 export default App;
--- Props-Lists/ProfileCard/src/Component/ProfileCard.jsx
+++ Props-Lists/ProfileCard/src/Component/ProfileCard.jsx
@@ -1,13 +1,11 @@
-function Profilecard() {
+function Profilecard(profile) {
+  console.log(profile);
   return (
     <div className="profile-card">
       <h2>ProfileCard</h2>
-      <img
-        src="https://i.pinimg.com/736x/e7/87/41/e7874123ba5b80d01e6bc1e58e545d3e.jpg"
-        alt="Hinata"
-      />
-      <p>Name: Tachibana Hinata</p>
-      <p>Role: girlfriend of Takemichi</p>
+      <img src={profile.profile.avatar} alt="Hinata" />
+      <p>Name: {profile.profile.name}</p>
+      <p>Role: {profile.profile.role}</p>
     </div>
   );
 }
