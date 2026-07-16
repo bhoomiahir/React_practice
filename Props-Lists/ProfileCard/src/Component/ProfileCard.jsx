a361065dc3086b629c16d2774b6b89239cb30488
Bhoomi Ahir <bhoomiahir.work@gmail.com>
2026-07-16 23:41:58 +0530

    destructured and gave props

--- Props-Lists/ProfileCard/src/Component/ProfileCard.jsx
+++ Props-Lists/ProfileCard/src/Component/ProfileCard.jsx
@@ -1,11 +1,11 @@
-function Profilecard(profile) {
+function Profilecard({ profile }) {
   console.log(profile);
   return (
     <div className="profile-card">
       <h2>ProfileCard</h2>
-      <img src={profile.profile.avatar} alt="Hinata" />
-      <p>Name: {profile.profile.name}</p>
-      <p>Role: {profile.profile.role}</p>
+      <img src={profile.avatar} alt="Hinata" />
+      <p>Name: {profile.name}</p>
+      <p>Role: {profile.role}</p>
     </div>
   );
 }
